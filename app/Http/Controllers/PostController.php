39cad2961f08f21c41fbffc4f9a5159b103c1a5d
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-05-25 17:40:49 -0400

    Criação de Controller, Migration e Model de POST

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdatePost;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request)
+    {
+        Post::create($request->all());
+
+        return redirect()->route('posts.index');
+    }
+
+    public function show($id)
+    {
+        if (!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+        }
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        if (!$post = Post::find($id))
+            return redirect()->route('posts.index');
+
+        $post->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('message','Post deletado com sucesso');
+    }
 }
